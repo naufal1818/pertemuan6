fbccfef7baf0bdecaf28cd7d35f8c152cb33be7a
naufal1818 <bagusnauval99gmail.com>
2023-11-10 21:47:31 +0700

    pertemuan6

---
+++ tugas/indek.php
@@ -0,0 +1,11 @@
+<?php
+$angka = 10;
+
+if ($angka > 0) {
+    echo "Angka $angka adalah positif.";
+} elseif ($angka < 0) {
+    echo "Angka $angka adalah negatif.";
+} else {
+    echo "Angka $angka adalah nol.";
+}
+?>
